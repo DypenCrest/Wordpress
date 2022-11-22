f1eea9c26d51258e128bd6ddb283f17cfb46b314
Dypen Sresta <dashadipen@gmail.com>
2022-11-22 14:45:58 +0545

    second commit

---
+++ wp-content/plugins/adminify/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a4dc4fc975ec16984b8141aa6320caaec300d8e6',
+        'name' => 'litonarefin/wp-adminify',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'litonarefin/wp-adminify' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a4dc4fc975ec16984b8141aa6320caaec300d8e6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
